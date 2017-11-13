f59e0bf0461b40501257241927e507ab8aad2de6
Yimeng Li <lym1108csu@gmail.com>
2017-11-12 20:19:21 -0500

    state model

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resources :foos, except: [:new, :edit]
     resources :bars, except: [:new, :edit]
     resources :cities, except: [:new, :edit]
+    resources :states, except: [:new, :edit]
   end
 
   get '/ui' => 'ui#index'
